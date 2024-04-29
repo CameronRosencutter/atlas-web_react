14aa74d8f378418d6c0228ea374f1a6707bf2f67
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-04-29 13:24:44 -0500

    the html now displays what is on the .js file

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,15 +1,13 @@
-// Import jQuery
-import $ from 'jquery';
-
-// Function to add paragraphs to the page body
-function addParagraphs() {
-    // Create and append three paragraphs with different text
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
-}
-
-// Call the function to add paragraphs when the DOM is ready
 $(document).ready(function() {
+    // Function to add paragraphs to the page body
+    function addParagraphs() {
+        // Create and append three paragraphs with different text
+        $('body').append('<p>Holberton Dashboard</p>');
+        $('body').append('<p>Dashboard data for the students</p>');
+        $('body').append('<p>Copyright - Holberton School</p>');
+    }
+
+    // Call the function to add paragraphs
     addParagraphs();
 });
+
