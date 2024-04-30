cce6ccd5bbdce223a1f3747bcc9fe4e3f6b3fde5
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-04-30 12:56:56 -0500

    more fixes

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,7 @@
 // Import jQuery
 import $ from 'jquery';
+// Import Lodash debounce function
+import debounce from 'lodash/debounce';
 
 // Function to add elements to the page body
 function addElements() {
@@ -17,7 +19,29 @@ function addElements() {
     $('body').append('<p>Copyright - Holberton School</p>');
 }
 
+// Function to update the counter and display the count
+function updateCounter() {
+    // Initialize count
+    let count = 0;
+
+    // Return a function to update counter and display count
+    return function() {
+        // Increment count
+        count++;
+
+        // Display count in paragraph element with id 'count'
+        $('#count').text(`${count} clicks on the button`);
+    };
+}
+
 // Call the function to add elements when the DOM is ready
 $(document).ready(function() {
+    // Add elements to the page
     addElements();
+
+    // Initialize updateCounter function
+    const updateCounterFunc = updateCounter();
+
+    // Bind debounce function to click event on the button
+    $('#startBtn').on('click', debounce(updateCounterFunc, 500));
 });
