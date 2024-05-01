ceae3e03cf7f5d8cbe8f368c6ddbaf5262ba6d73
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-01 15:04:21 -0500

    npx webpack now works

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'production', // Set webpack mode to production
@@ -27,21 +26,11 @@ module.exports = {
   plugins: [
     // HTML Webpack Plugin for generating HTML file
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Path to your HTML template
+      template: './public/index.html', // Path to your HTML template
     }),
     // Mini CSS Extract Plugin for extracting CSS into separate files
     new MiniCssExtractPlugin({
       filename: 'styles.css',
-    }),
-    // Image Minimizer Plugin for optimizing images
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        plugins: [
-          ['imagemin-mozjpeg', { quality: 50 }],
-          ['imagemin-pngquant', { quality: [0.5, 0.7] }],
-        ],
-      },
-      deleteOriginalAssets: false, // Keep the original images after optimization
     }),
   ],
 };
