523c9f4849edccae143e8ddcc68a32630215b9fc
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-01 14:36:30 -0500

    making some changes

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,64 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
+  mode: 'production', // Set webpack mode to production
+  entry: './src/index.js', // Entry point of your application
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
+      // Configuration for processing CSS files
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      // Configuration for processing images
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext][query]',
-        },
       },
     ],
   },
   plugins: [
+    // HTML Webpack Plugin for generating HTML file
     new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
+      template: './src/index.html', // Path to your HTML template
     }),
+    // Mini CSS Extract Plugin for extracting CSS into separate files
     new MiniCssExtractPlugin({
-      filename: 'main.css',
+      filename: 'styles.css',
     }),
-    new OptimizeCssAssetsPlugin(),
+    // Image Minimizer Plugin for optimizing images
     new ImageMinimizerPlugin({
       minimizerOptions: {
         plugins: [
           ['imagemin-mozjpeg', { quality: 50 }],
-          ['imagemin-pngquant', { quality: [0.5, 0.75] }],
+          ['imagemin-pngquant', { quality: [0.5, 0.7] }],
         ],
       },
+      deleteOriginalAssets: false, // Keep the original images after optimization
     }),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-    ],
-  },
 };
