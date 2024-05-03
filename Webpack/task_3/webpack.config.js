af3154a32c94c1357a86624451576c5ae8dbd8c5
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-03 14:43:06 -0500

    fixing up the webpack

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: './js/dashboard_main',
   performance: {
     maxAssetSize: 1000000,
