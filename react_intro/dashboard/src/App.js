747cb45ec5934d92f21933b5e26b6bcabfa74c96
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-04 17:17:13 -0500

    lots of changes

---
+++ react_intro/dashboard/src/App.js
@@ -0,0 +1,14 @@
+import logo from './HL.ico';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>School Dashboard</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
