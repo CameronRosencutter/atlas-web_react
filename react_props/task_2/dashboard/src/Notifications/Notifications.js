880c08b6219bcb072ce22ea797df926e4a82543c
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-07 09:46:24 -0500

    Finished task_2

---
+++ react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem'; // Make sure to import NotificationItem
+import './Notifications.css';
+
+const Notifications = () => {
+  const handleClick = () => {
+    console.log('Close button has been clicked');
+  };
+
+  return (
+    <div className="notifications">
+      <button
+        onClick={handleClick}
+        style={{ position: 'absolute', top: '10px', right: '10px' }}
+        aria-label="Close"
+      >
+        x
+      </button>
+      <p>Here is the list of requirements</p>
+      <ul>
+        <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New resume available" />
+        <NotificationItem type="urgent" html={{ __html: getLatestNotification() }} />
+      </ul>
+    </div>
+  );
+};
+
+export default Notifications;
