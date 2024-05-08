de7d28fbb107708c2eb635f6da252e64da07985f
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-08 14:02:56 -0500

    doing task_1

---
+++ React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import courses from '../CourseList/coursesData';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false
+    };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+    }
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { displayDrawer } = this.state;
+
+    return (
+      <div className="App">
+        <Notifications displayDrawer={displayDrawer} />
+        <Header />
+        {isLoggedIn ? <CourseList listCourses={courses} /> : <Login />}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: true,
+  logOut: () => {}, // Empty function as default
+};
+
+export default App;
