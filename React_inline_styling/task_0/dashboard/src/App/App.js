4e8705cfaad4c39d5460e955c05389b8233c53dd
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-14 12:47:26 -0500

    working task1

---
+++ React_inline_styling/task_0/dashboard/src/App/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import courses from '../CourseList/coursesData';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayDrawer: false
+    };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+    }
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { displayDrawer } = this.state;
+
+    return (
+      <div className="App">
+        <Notifications displayDrawer={displayDrawer} />
+        <Header />
+        <BodySectionWithMarginBottom title="Log in to continue">
+          <Login />
+        </BodySectionWithMarginBottom>
+        <BodySection title="News from the School">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rhoncus velit ac lorem consequat, nec tempor justo dictum. Nam id turpis non eros auctor placerat. Nulla facilisi. Proin quis orci a diam rutrum viverra at non sem. Sed eget arcu id tortor scelerisque viverra ac nec odio.</p>
+        </BodySection>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {}, // Empty function as default
+};
+
+export default App;
