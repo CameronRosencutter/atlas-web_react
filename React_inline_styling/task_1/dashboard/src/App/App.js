02e094280bc1ac0f5c84467e6a96adbd7b157b01
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-14 15:09:52 -0500

    Start task1

---
+++ React_inline_styling/task_1/dashboard/src/App/App.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import BodySection from '../BodySection/BodySection';
+
+const listCourses = [
+  {
+    id: 1,
+    name: 'ES6',
+    credit: 60,
+  },
+  {
+    id: 2,
+    name: 'Webpack',
+    credit: 20,
+  },
+  {
+    id: 3,
+    name: 'React',
+    credit: 40,
+  },
+];
+
+const listNotifications = [
+  {
+    id: 1,
+    type: 'default',
+    value: 'Default Notification',
+  },
+  {
+    id: 2,
+    type: 'urgent',
+    value: 'Urgent Notification',
+  },
+  {
+    id: 3,
+    type: 'urgent',
+    html: { __html: '<b>Html</b> notification' },
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: this.props.isLoggedIn,
+      displayDrawer: false
+    };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    const { logOut } = this.props;
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      logOut();
+      this.setState({ isLoggedIn: false });
+    } else if (event.ctrlKey && event.key === 'l') {
+      this.setState({ isLoggedIn: true, displayDrawer: true });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <div className="root-notifications"></div>
+        <div className="App-header">
+          <Header />
+          {this.state.displayDrawer && <Notifications listNotifications={listNotifications} />}
+        </div>
+        <div className="App-body">
+          <main>
+            <BodySectionWithMarginBottom>
+              {this.state.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+            </BodySectionWithMarginBottom>
+            <BodySection>
+              <h2>News from the School</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo provident possimus numquam autem.</p>
+            </BodySection>
+          </main>
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {}, // Empty function as default
+};
+
+export default App;
---
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const headerRowStyle = {
+  backgroundColor: '#deb5b545',
+};
+
+const rowStyle = {
+  backgroundColor: '#f5f5f5ab',
+};
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  if (isHeader) {
+    if (textSecondCell === null) {
+      return (
+        <tr>
+          <th  colSpan="2">{textFirstCell}</th>
+        </tr>
+      );
+    } else {
+      return (
+        <tr>
+          <th style={headerRowStyle}>{textFirstCell}</th>
+          <th style={headerRowStyle}>{textSecondCell}</th>
+        </tr>
+      );
+    }
+  } else {
+    return (
+      <tr>
+        <td style={rowStyle}>{textFirstCell}</td>
+        <td>{textSecondCell}</td>
+      </tr>
+    );
+  }
+}
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string,
+};
+
+export default CourseListRow;
