bed5de9f8268b7fe5fcd536a71fdc6fb0ae82463
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-19 23:20:40 -0500

    accidentally updated wrong task

--- react_state/task_2/dashboard/src/Footer/Footer.js
+++ react_state/task_2/dashboard/src/Footer/Footer.js
@@ -1,33 +1,13 @@
-import React, { useContext } from 'react';
-import { StyleSheet, css } from 'aphrodite';
-import AppContext from '../App/AppContext';
+import React from 'react';
+import './footer.css';
+import { getFullYear, getFooterCopy } from '../utils/utils.js';
 
-const styles = StyleSheet.create({
-  footer: {
-    borderTop: '7px solid rgb(255, 0, 0)',
-    display: 'flex',
-    alignItems: 'center',
-    padding: '20px',
-    justifyContent: 'center',
-  },
-  link: {
-    marginLeft: '10px',
-  },
-});
-
-const Footer = () => {
-  const { user } = useContext(AppContext);
-
+function Footer() {
   return (
-    <div className={css(styles.footer)}>
-      <p>&copy; 2020 Holberton School</p>
-      {user.isLoggedIn && (
-        <p className={css(styles.link)}>
-          <a href="/contact">Contact us</a>
-        </p>
-      )}
-    </div>
+    <footer>
+      <p>{getFullYear()} - {getFooterCopy(false)}</p>
+    </footer>
   );
-};
+}
 
 export default Footer;
