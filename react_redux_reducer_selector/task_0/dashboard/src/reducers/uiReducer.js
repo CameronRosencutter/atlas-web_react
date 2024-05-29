3072905a1bc5c265ed1da7c7d77c16a2321e1fc4
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-05-29 13:51:02 -0500

    finishing task1

--- react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
+++ react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -1,50 +1,34 @@
-import { 
-    DISPLAY_NOTIFICATION_DRAWER, 
-    HIDE_NOTIFICATION_DRAWER, 
-    LOGIN_SUCCESS, 
-    LOGIN_FAILURE, 
-    LOGOUT 
-  } from '../actions/uiActionTypes';
-  
-  // Initial state
-  const initialState = {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {}
-  };
-  
-  // Reducer function
-  const uiReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case DISPLAY_NOTIFICATION_DRAWER:
-        return {
-          ...state,
-          isNotificationDrawerVisible: true
-        };
-      case HIDE_NOTIFICATION_DRAWER:
-        return {
-          ...state,
-          isNotificationDrawerVisible: false
-        };
-      case LOGIN_SUCCESS:
-        return {
-          ...state,
-          isUserLoggedIn: true
-        };
-      case LOGIN_FAILURE:
-        return {
-          ...state,
-          isUserLoggedIn: false
-        };
-      case LOGOUT:
-        return {
-          ...state,
-          isUserLoggedIn: false
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default uiReducer;
-  
+// src/reducers/uiReducer.js
+import { Map } from 'immutable';
+import {
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT
+} from '../actions/uiActionTypes';
+
+const initialState = Map({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {}
+});
+
+const uiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true);
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false);
+    case LOGIN_SUCCESS:
+      return state.set('isUserLoggedIn', true);
+    case LOGIN_FAILURE:
+      return state.set('isUserLoggedIn', false);
+    case LOGOUT:
+      return state.set('isUserLoggedIn', false);
+    default:
+      return state;
+  }
+};
+
+export default uiReducer;
--- react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
+++ react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.test.js
@@ -1,79 +1,51 @@
+// src/reducers/uiReducer.test.js
+import { Map } from 'immutable';
 import uiReducer from './uiReducer';
-import { 
-  DISPLAY_NOTIFICATION_DRAWER, 
-  HIDE_NOTIFICATION_DRAWER, 
-  LOGIN_SUCCESS, 
-  LOGIN_FAILURE, 
-  LOGOUT 
-} from '../actions/uiActionTypes';
-
-// Initial state for comparison
-const initialState = {
-  isNotificationDrawerVisible: false,
-  isUserLoggedIn: false,
-  user: {}
-};
+import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../actions/uiActionTypes';
 
 describe('uiReducer', () => {
+  const initialState = Map({
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {}
+  });
+
   it('should return the initial state when no action is passed', () => {
-    expect(uiReducer(undefined, {})).toEqual(initialState);
+    expect(uiReducer(undefined, {}).toJS()).toEqual(initialState.toJS());
   });
 
   it('should return the initial state when the action SELECT_COURSE is passed', () => {
-    const action = { type: 'SELECT_COURSE' };
-    expect(uiReducer(undefined, action)).toEqual(initialState);
+    expect(uiReducer(undefined, { type: 'SELECT_COURSE' }).toJS()).toEqual(initialState.toJS());
   });
 
-  it('should handle DISPLAY_NOTIFICATION_DRAWER action', () => {
+  it('should handle DISPLAY_NOTIFICATION_DRAWER', () => {
     const action = { type: DISPLAY_NOTIFICATION_DRAWER };
-    const expectedState = {
-      ...initialState,
-      isNotificationDrawerVisible: true
-    };
-    expect(uiReducer(undefined, action)).toEqual(expectedState);
+    const expectedState = initialState.set('isNotificationDrawerVisible', true);
+    expect(uiReducer(initialState, action).toJS()).toEqual(expectedState.toJS());
   });
 
-  it('should handle HIDE_NOTIFICATION_DRAWER action', () => {
-    const initialStateModified = {
-      ...initialState,
-      isNotificationDrawerVisible: true
-    };
+  it('should handle HIDE_NOTIFICATION_DRAWER', () => {
     const action = { type: HIDE_NOTIFICATION_DRAWER };
-    const expectedState = {
-      ...initialState,
-      isNotificationDrawerVisible: false
-    };
-    expect(uiReducer(initialStateModified, action)).toEqual(expectedState);
+    const currentState = initialState.set('isNotificationDrawerVisible', true);
+    const expectedState = initialState.set('isNotificationDrawerVisible', false);
+    expect(uiReducer(currentState, action).toJS()).toEqual(expectedState.toJS());
   });
 
-  it('should handle LOGIN_SUCCESS action', () => {
+  it('should handle LOGIN_SUCCESS', () => {
     const action = { type: LOGIN_SUCCESS };
-    const expectedState = {
-      ...initialState,
-      isUserLoggedIn: true
-    };
-    expect(uiReducer(undefined, action)).toEqual(expectedState);
+    const expectedState = initialState.set('isUserLoggedIn', true);
+    expect(uiReducer(initialState, action).toJS()).toEqual(expectedState.toJS());
   });
 
-  it('should handle LOGIN_FAILURE action', () => {
+  it('should handle LOGIN_FAILURE', () => {
     const action = { type: LOGIN_FAILURE };
-    const expectedState = {
-      ...initialState,
-      isUserLoggedIn: false
-    };
-    expect(uiReducer(undefined, action)).toEqual(expectedState);
+    const expectedState = initialState.set('isUserLoggedIn', false);
+    expect(uiReducer(initialState, action).toJS()).toEqual(expectedState.toJS());
   });
 
-  it('should handle LOGOUT action', () => {
-    const initialStateModified = {
-      ...initialState,
-      isUserLoggedIn: true
-    };
+  it('should handle LOGOUT', () => {
     const action = { type: LOGOUT };
-    const expectedState = {
-      ...initialState,
-      isUserLoggedIn: false
-    };
-    expect(uiReducer(initialStateModified, action)).toEqual(expectedState);
+    const expectedState = initialState.set('isUserLoggedIn', false);
+    expect(uiReducer(initialState, action).toJS()).toEqual(expectedState.toJS());
   });
 });
