97d3ebcecfa8271c29328a0814509d7bce753e54
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-01 20:39:44 -0500

    finish task5

---
+++ react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.js
@@ -0,0 +1,14 @@
+// src/selectors/notificationSelector.js
+import { createSelector } from 'reselect';
+
+// Selector to get the filter type
+export const filterTypeSelected = (state) => state.get('filter');
+
+// Selector to get all notifications
+export const getNotifications = (state) => state.get('notifications');
+
+// Selector to get unread notifications
+export const getUnreadNotifications = createSelector(
+  getNotifications,
+  (notifications) => notifications.filter(notification => !notification.isRead)
+);
