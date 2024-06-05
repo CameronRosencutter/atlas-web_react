779572fe233b940511c73316afda8574791f4a23
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-05 16:01:13 -0500

    update index.js

--- React Redux Connectors and Providers/task_3/dashboard/src/index.js
+++ React Redux Connectors and Providers/task_3/dashboard/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import App from './App/App';
-import uiReducer from './reducers/uiReducer';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import App from './App';
 
-const store = createStore(uiReducer);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
